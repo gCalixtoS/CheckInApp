31a1c35b00c28fe0d72ec4d0da548e77675c8f87
gCalixtoS <gustavo.calixtosouza@gmail.com>
2020-06-03 19:35:34 +0000

    Ui Added

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
 import React from 'react'
 import logo from './assets/SAP_scrn_R.png'
+
+import {GraphData} from './components/Graph/GraphData'
 import "@ui5/webcomponents-fiori/dist/ShellBar"
 import Tabs from './components/Tabs/Tabs'
+
 import axios from 'axios'
+import { withAuth } from './msal/MsalAuthProvider'
 
-const url = 'https://sap-presales-cloud--neo-foundry--sappsbr-dev-graph-tutorial-srv.cfapps.eu10.hana.ondemand.com'
-const headers = {
-    'user': 'sb-clone1e2b1f2b89674b55afa155fbddc97144!b14870|destination-xsappname!b404',
-    'password': 'dN3Cfc9sb+wtXyXg2CtM913jGtw=',
-}
-var login = () => {
-    axios.get(url, {headers})
-        .then(resp => console.log(resp))
-}
-
-function App() {
-    login()
-    return (
-        <div className="App">
-            <ui5-shellbar
-                primary-title="Office Check-in App"
-                logo={logo}
-            >
-            </ui5-shellbar>
-            <Tabs />
-        </div>
-    );
+class RootApp extends React.Component {
+    
+    render() {
+        return (
+            <div className="App">
+                <ui5-shellbar
+                    primary-title="Office Check-in App"
+                    logo={logo}
+                >
+                </ui5-shellbar>
+                <Tabs />
+            </div>
+        )
+    }
 }
 
-export default App;
+export const App = withAuth(RootApp);
